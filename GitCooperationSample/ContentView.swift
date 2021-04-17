024a08eb20cc27207ec5b3b42cc07a51424af4b4
Tsubasa <mmst0410@gmail.com>
2021-04-17 17:08:34 +0900

    Initial Commit

---
+++ GitCooperationSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitCooperationSample
+//
+//  Created by 塩貝翼 on 2021/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitCooperationSample/GitCooperationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitCooperationSampleApp.swift
+//  GitCooperationSample
+//
+//  Created by 塩貝翼 on 2021/04/17.
+//
+
+import SwiftUI
+
+@main
+struct GitCooperationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
